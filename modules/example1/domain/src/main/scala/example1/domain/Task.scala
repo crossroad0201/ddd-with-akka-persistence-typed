1333c6e856e8dc52be76e26cb92620728e5398ef
Yohei TSUJI <crossroad0201@gmail.com>
2022-02-05 17:32:50 +0900

    Implement domain for example1.

---
+++ modules/example1/domain/src/main/scala/example1/domain/Task.scala
@@ -0,0 +1,45 @@
+package example1.domain
+
+case class Task(
+    id: TaskId,
+    subject: Subject,
+    status: Status
+) {
+  def canEditSubject: Boolean =
+    status != Status.Done
+
+  def editSubject(newSubject: Subject): Task = {
+    require(canEditSubject)
+    copy(subject = newSubject)
+  }
+
+  def canToDone: Boolean =
+    status != Status.Done
+
+  def toDone: Task = {
+    require(canToDone)
+    copy(status = Status.Done)
+  }
+
+  def canReturnToTodo: Boolean =
+    status != Status.Todo
+
+  def returnToTodo: Task = {
+    require(canReturnToTodo)
+    copy(status = Status.Todo)
+  }
+}
+object Task {
+  def create(id: TaskId, subject: Subject): Task =
+    Task(id, subject, Status.Todo)
+}
+
+case class TaskId(value: String)
+
+case class Subject(value: String)
+
+sealed abstract class Status(val value: String)
+object Status {
+  case object Todo extends Status("TODO")
+  case object Done extends Status("DONE")
+}
